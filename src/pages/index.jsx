b924474c1a962097ccfb385b3b375d1f187664db
Avétis KAZARIAN <avetis.k@gmail.com>
2023-12-04 16:36:42 +0100

    feat: front page todos list

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,3 +1,4 @@
+import axios from "axios"
 import { useEffect, useState } from "react"
 
 const IndexPage = () => {
@@ -5,11 +6,35 @@ const IndexPage = () => {
 
   useEffect(() => {
     ;(async () => {
-      // axios
+      const { data } = await axios("http://localhost:3000/api/todos")
+
+      setTodos(data)
     })()
   }, [])
 
-  return <table>...</table>
+  return (
+    <table className="w-full">
+      <thead>
+        <tr>
+          {["#", "Description", "Done", "Category"].map((label) => (
+            <td key={label} className="p-4 bg-slate-300">
+              {label}
+            </td>
+          ))}
+        </tr>
+      </thead>
+      <tbody>
+        {todos.map(({ id, description, isDone, category: { name } }) => (
+          <tr key={id} className="even:bg-slate-100">
+            <td className="p-4">{id}</td>
+            <td className="p-4">{description}</td>
+            <td className="p-4">{isDone ? "✅" : "❌"}</td>
+            <td className="p-4">{name}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  )
 }
 
 export default IndexPage
